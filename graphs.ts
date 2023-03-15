878f9bd3eaf819905500b44d734c5f02ae117d7c
nonfinity <poe.mike@gmail.com>
2023-03-15 00:05:48 -0400

    Preparing the foursquare tests. Base 4sq layout set up, ticking beep and buttons working. Next is add simulation and then live graphing

---
+++ graphs.ts
@@ -0,0 +1,10 @@
+/**
+ * Needed Graphs:
+ * 1. Force directed graph
+ * 2. Line graph for prices
+ * 3. Line graph for inventory (may use same logic as #2)
+ * 
+ * Goal: at most, expose a single function for each one
+ * 
+ */
+
